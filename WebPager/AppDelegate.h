9a156a66161d1522367ad1bed8762ffd2ce1d133
Ilter Cengiz <me@iltercengiz.info>
2014-09-16 13:37:23 +0300

    Initial Commit

---
+++ WebPager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebPager
+//
+//  Created by Ilter Cengiz on 16/09/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebPager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebPager
+//
+//  Created by Ilter Cengiz on 16/09/14.
+//  Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
